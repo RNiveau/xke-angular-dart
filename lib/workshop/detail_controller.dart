04fd4b8389e68bb12314c1544e45e45918b58e94
yannick grenzinger <yannick.grenzinger@gmail.com>
2014-05-05 13:44:47 +0200

    Empty detail_controller

--- lib/workshop/detail_controller.dart
+++ lib/workshop/detail_controller.dart
@@ -1,21 +1,7 @@
 library detail_controller;
 
 import 'package:angular/angular.dart';
-import 'dart:async';
+import 'logs_service.dart';
 
 import 'log.dart';
-
-
-@Controller(
-    selector: '[detail-ctrl]',
-    publishAs: 'detailCtrl')
-class DetailController {
-  
-  Log log;
-  
-  DetailController(Http _http, RouteProvider _routeProvider) {
-    
-  }
-  
-}
 
